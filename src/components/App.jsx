963ec5033135cd4058a7b9c958348cd2765bff46
Karan Sheth <karan.sheth@nutanix.com>
2017-01-31 23:37:31 +0530

    adds editor tabs and remembers their values in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,49 @@
+import './app.scss';
 
 import React from 'react';
 import PureComponent from 'react-pure-render/component';
+import cns from 'classnames';
 
 import { parse } from 'utils/parse';
 
 import Entry from './Entry';
 
+const CURRENT_INDEX = '@@current_index@@';
+const OBJECTS = '@@objects@@';
+
+let __objects = localStorage.getItem(OBJECTS);
+let __currentIndex = localStorage.getItem(CURRENT_INDEX) || 0;
+if (__objects) {
+    __objects = JSON.parse(__objects);
+    if (__currentIndex !== undefined && __objects[__currentIndex]) {
+        parse(__objects[__currentIndex]);
+    }
+} else {
+    __objects = [];
+}
+
 export default class App extends PureComponent {
     state = {
-        obj: null,
+        objects: __objects,
+        currentIndex: __currentIndex,
         isEditing: false,
     }
 
     render() {
-        const { obj, isEditing } = this.state;
+        const { objects, currentIndex, isEditing } = this.state;
+        const obj = objects[currentIndex];
+
+        const editorTabs = (
+            <div className="editor-tabs">
+                TABS
+                {[0, 1, 2, 3, 4].map(i =>
+                    <button key={i}
+                        type="button"
+                        className={cns({'editor-tab-active': i === currentIndex})}
+                        onClick={this.onChangeTab.bind(this, i)}>{i + 1}</button>
+                )}
+            </div>
+        );
 
         if (!obj || isEditing) {
             const taStyle = {
@@ -24,6 +54,7 @@ export default class App extends PureComponent {
                 <div>
                     <div className="actions">
                         <button type="button" onClick={this.onParse}>Parse</button>
+                        {editorTabs}
                     </div>
                     <textarea name="obj_string"
                         placeholder="JSON string goes here..."
@@ -38,11 +69,17 @@ export default class App extends PureComponent {
                 <div>
                     <div className="actions">
                         <button type="button" onClick={this.onEdit}>Edit</button>
+                        {editorTabs}
                     </div>
                     <Entry value={obj} />
                 </div>
             );
         }
+    }
+
+    onChangeTab = (currentIndex) => {
+        localStorage.setItem(CURRENT_INDEX, currentIndex);
+        this.setState({currentIndex});
     }
 
     onEdit = () => {
@@ -52,7 +89,14 @@ export default class App extends PureComponent {
     onParse = () => {
         try {
             const obj = parse(this.refs.textarea.value);
-            this.setState({obj, isEditing: false});
+            const { objects, currentIndex } = this.state;
+            const newObjects = [...objects];
+            newObjects[currentIndex] = obj;
+            localStorage.setItem(OBJECTS, JSON.stringify(newObjects));
+            this.setState({
+                objects: newObjects,
+                isEditing: false,
+            });
         } catch (e) {
             alert(e);
         }
